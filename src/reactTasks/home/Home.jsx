1c77a3b27792354ce2f604882a78432e6a26b4b6
ManeeshChikoti <96185794+ManeeshChikoti@users.noreply.github.com>
2023-01-21 03:32:25 +0530

    Update Home.jsx

--- src/reactTasks/home/Home.jsx
+++ src/reactTasks/home/Home.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
 export default function Home() {
   return (
-    <div className='home'>
-        <span>Task 1</span>
-        <span>Task 2</span>
-        <span>Task 3</span>
-        <span>Task 4</span>
-        <span>Task 5</span>
+    <div className="home">
+      <h2>AdMAVIN Tasks</h2>
+      <div className="homewrapper">
+        <Link className="link" to="/task1">
+          <span>Link to Task 1</span>
+        </Link>
+        <Link to="/task2" className="link">
+          <span> Link to Task 2</span>
+        </Link>
+        <Link to="/task3" className="link">
+          <span>Link to Task 3</span>
+        </Link>
+        <Link to="/task4" className="link">
+          <span> Link to Task 4</span>
+        </Link>
+        <Link to="/task5" className="link">
+          <span>Link to Task 5</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
