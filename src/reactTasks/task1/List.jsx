a49355140a2319f3c333686a933ab7738b022de8
ManeeshChikoti <96185794+ManeeshChikoti@users.noreply.github.com>
2023-02-01 15:08:12 +0530

    Update List.jsx

--- src/reactTasks/task1/List.jsx
+++ src/reactTasks/task1/List.jsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React  from "react";
 
-export default function List({item,i,setSelect,select}) {
-
+export default function List({item,i,setSelect,select,selectbg,setSelectbg}) {
+ 
   const handleSelect = (item) => {
     if (!select.includes(item)) {
       setSelect((prev) => [...prev, item]); 
+      setSelectbg(true);
     }
   };
   return (
     <li
-     className="li"
+     className={"li " +(selectbg && select.includes(item)? 'bg':'')}
       key={i}
       onClick={() => {
         handleSelect(item);
