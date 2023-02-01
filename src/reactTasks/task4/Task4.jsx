1128c12fc3982e1ac709926b5c5aab3b3f8eec8e
ManeeshChikoti <96185794+ManeeshChikoti@users.noreply.github.com>
2023-02-01 15:09:56 +0530

    Update Task4.jsx

--- src/reactTasks/task4/Task4.jsx
+++ src/reactTasks/task4/Task4.jsx
@@ -2,14 +2,17 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import "./task4.css";
 
-const Square = ({num,index,setscore}) => {
+const Square = ({num,index,setscore,setPoints,setPointsLost}) => {
 
-    const handleClick=()=>{
+    const handleClick=( )=>{
         if(num===index){
           setscore((prev)=>prev+5);
+          setPoints((prev)=>(prev+1));
+         
         }
         else{
             setscore((prev)=>prev-2.5);
+            setPointsLost((prev)=>(prev+1));
         }
     
     }
@@ -21,6 +24,9 @@ const Square = ({num,index,setscore}) => {
 export default function Task4() {
   const [index, setIndex] = useState(0);
   const [score,setscore]=useState(0);
+   const[pointsGain,setPoints]=useState(0)
+  const[pointsLost,setPointsLost]=useState(0)
+ 
   const [finished,setFinished]=useState(false);
 
   useEffect(() => {
@@ -40,27 +46,28 @@ export default function Task4() {
    console.log(index)
 
   return (
-    <div className="Task4">
-        <h1>Score:{score}</h1>
+    <div className="Task4"> 
+        <h2>Score:{score},rightCount:{pointsGain},WrongCount:{pointsLost}</h2>
+       {!finished?(
       <div className="gameWrapper">
         <div className="row">
-         <Square num={1} index={index} setscore={setscore} />
-         <Square num={2}  index={index}  setscore={setscore}  />
-         <Square num={3}  index={index} setscore={setscore} />
+         <Square num={1} index={index} setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+         <Square num={2}  index={index}  setscore={setscore} setPoints={setPoints} setPointsLost={setPointsLost}/>
+         <Square num={3}  index={index} setscore={setscore} setPoints={setPoints}setPointsLost={setPointsLost}/>
        
         </div>
         <div className="row">
-        <Square num={4}  index={index}  setscore={setscore}/>
-         <Square num={5}  index={index}  setscore={setscore}/>
-         <Square num={6}  index={index}  setscore={setscore}/>
+        <Square num={4}  index={index}  setscore={setscore} setPoints={setPoints} setPointsLost={setPointsLost}/>
+         <Square num={5}  index={index}  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+         <Square num={6}  index={index}  setscore={setscore} setPoints={setPoints} setPointsLost={setPointsLost}/>
         </div>
         <div className="row">
-        <Square num={7}  index={index}  setscore={setscore}/>
-         <Square num={8}  index={index }  setscore={setscore}/>
-         <Square num={9}  index={index}  setscore={setscore}/>
+        <Square num={7}  index={index}  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+         <Square num={8}  index={index }  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+         <Square num={9}  index={index}  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
         </div>
-      </div>
-      {finished&&<h3>Game Finished!!!</h3>}
+      </div>):<h3>Game Finished!!!</h3>}
     </div>
   );
 }
+
