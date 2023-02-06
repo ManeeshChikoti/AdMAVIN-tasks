e4c1694279fe8ca12b28c18a27ee0cc5f7a942bc
ManeeshChikoti <96185794+ManeeshChikoti@users.noreply.github.com>
2023-02-06 14:36:09 +0530

    Update Task4.jsx

--- src/reactTasks/task4/Task4.jsx
+++ src/reactTasks/task4/Task4.jsx
@@ -2,28 +2,28 @@ import React, { useEffect } from "react";
 import { useState } from "react";
 import "./task4.css";
 
-const Square = ({num,index,setscore,setPoints,setPointsLost}) => {
+const Square = ({num,index,setScore,setPoints,setPointsLost}) => {
 
     const handleClick=( )=>{
         if(num===index){
-          setscore((prev)=>prev+5);
+          setScore((prev)=>prev+5);
           setPoints((prev)=>(prev+1));
          
         }
         else{
-            setscore((prev)=>prev-2.5);
+            setScore((prev)=>prev-2.5);
             setPointsLost((prev)=>(prev+1));
         }
     
     }
-  return <div className="col"   onClick={handleClick}>
+  return <div className="items"   onClick={handleClick}>
     {num===index&&(<h1 >HIT</h1>)}
   </div>;
 };
 
 export default function Task4() {
   const [index, setIndex] = useState(0);
-  const [score,setscore]=useState(0);
+  const [score,setScore]=useState(0);
    const[pointsGain,setPoints]=useState(0)
   const[pointsLost,setPointsLost]=useState(0)
  
@@ -43,31 +43,44 @@ export default function Task4() {
 
     return () => clearInterval(indexRandom);
   }, []);
-   console.log(index)
 
   return (
     <div className="Task4"> 
         <h2>Score:{score},rightCount:{pointsGain},WrongCount:{pointsLost}</h2>
-       {!finished?(
-      <div className="gameWrapper">
-        <div className="row">
-         <Square num={1} index={index} setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
-         <Square num={2}  index={index}  setscore={setscore} setPoints={setPoints} setPointsLost={setPointsLost}/>
-         <Square num={3}  index={index} setscore={setscore} setPoints={setPoints}setPointsLost={setPointsLost}/>
-       
-        </div>
-        <div className="row">
-        <Square num={4}  index={index}  setscore={setscore} setPoints={setPoints} setPointsLost={setPointsLost}/>
-         <Square num={5}  index={index}  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
-         <Square num={6}  index={index}  setscore={setscore} setPoints={setPoints} setPointsLost={setPointsLost}/>
-        </div>
-        <div className="row">
-        <Square num={7}  index={index}  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
-         <Square num={8}  index={index }  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
-         <Square num={9}  index={index}  setscore={setscore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
-        </div>
-      </div>):<h3>Game Finished!!!</h3>}
+       {!finished?
+        <div className="game">
+      {
+        [...Array(9)].map((num,i)=>{
+        return  <Square num={i+1}key={i}  index={index}  setScore={setScore} setPoints={setPoints} setPointsLost={setPointsLost}/>
+         })
+      }
+    </div>
+      :<h3>Game Finished!!!</h3>}
     </div>
   );
 }
+
+// removed  repetitive code
+
+    //    (
+    //   <div className="gameWrapper">
+    //     <div className="row">
+    //  {
+    //     [...Array(3)].map((num,i)=>{
+    //       console.log(i)
+    //      return  <Square num={i+1}key={i}  index={index}  setScore={setScore} setPoints={setPoints} setPointsLost={setPointsLost}/>
+    //     })
+    //  }
+    //     </div>
+    //     <div className="row">
+    //     <Square num={4}  index={index}  setScore={setScore} setPoints={setPoints} setPointsLost={setPointsLost}/>
+    //      <Square num={5}  index={index}  setScore={setScore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+    //      <Square num={6}  index={index}  setScore={setScore} setPoints={setPoints} setPointsLost={setPointsLost}/>
+    //     </div>
+    //     <div className="row">
+    //     <Square num={7}  index={index}  setScore={setScore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+    //      <Square num={8}  index={index }  setScore={setScore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+    //      <Square num={9}  index={index}  setScore={setScore}  setPoints={setPoints} setPointsLost={setPointsLost}/>
+    //     </div>
+    //   </div>)
 
